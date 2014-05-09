3383459c8cbc91403b6fa9eb2e28ae0c63d07ff5
Nate Beatty <nate.beatty@gmail.com>
2014-05-09 11:53:06 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * main.js
+ *
+ * Nate Beatty | May 9, 2014
+ *
+ * All of the logic for rendering graphics using ThreeJS is contained within
+ * this main JS file.
+ */
+
+// Define some global variables
+var aspect = window.innerWidth / window.innerHeight;
+var assetpath = 'shed/Shed.obj';
+
+var scene, camera, renderer, controls, clock, model;
+
+function init() {
+
+}
+
+function buildScene() {
+
+}
